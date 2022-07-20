7ba916fb17024ef2f410f891a7fc0283a17e706d
Ashwin Pugalia <98446038+Ashwin-1709@users.noreply.github.com>
2022-07-20 15:17:45 +0530

    Update spf.cpp

--- Combinatorics/spf.cpp
+++ Combinatorics/spf.cpp
@@ -2,7 +2,7 @@ vector<int>spf;
 void prime_sieve(int MAX_PR) {
     spf.assign(MAX_PR + 1 , 0);
     iota(all(spf) , 0LL);
-    for(int i = 4 ; i <= MAX_PR ; i+= 2) spf[i] = 2;
+    for(int i = 2 ; i <= MAX_PR ; i+= 2) spf[i] = 2;
     for(int i = 3 ; i * i <= MAX_PR ; i++) {
         if(spf[i] == i) {
             for(int j = i * i ; j <= MAX_PR ; j+=i) {

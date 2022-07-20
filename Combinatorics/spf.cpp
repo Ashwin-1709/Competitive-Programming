3392bf5278ee19da8e13bce37cdcc237407f0980
Ashwin-1709 <ashwinpugalia1@gmail.com>
2022-07-20 15:16:00 +0530

    spf added

---
+++ Combinatorics/spf.cpp
@@ -0,0 +1,14 @@
+vector<int>spf;
+void prime_sieve(int MAX_PR) {
+    spf.assign(MAX_PR + 1 , 0);
+    iota(all(spf) , 0LL);
+    for(int i = 4 ; i <= MAX_PR ; i+= 2) spf[i] = 2;
+    for(int i = 3 ; i * i <= MAX_PR ; i++) {
+        if(spf[i] == i) {
+            for(int j = i * i ; j <= MAX_PR ; j+=i) {
+                if(spf[j] == j) spf[j] = i;
+            }
+        }
+    }
+ 
+}

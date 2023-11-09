6a378f81ba71b0fa12275380e68b6d91b01aa37f
Milind Jain <75107993+jainmilind@users.noreply.github.com>
2023-11-09 19:06:59 +0530

    Add Functions and Refactored
    Add nPr, nCr functions
    Add get prime factorization function
    Refactored Code

--- Combinatorics/spf.cpp
+++ Combinatorics/spf.cpp
@@ -1,13 +1,22 @@
-vector<int>spf;
+vector<int> spf;
 void prime_sieve(int MAX_PR) {
-    spf.assign(MAX_PR + 1 , 0);
-    iota(all(spf) , 0LL);
-    for(int i = 2 ; i <= MAX_PR ; i+= 2) spf[i] = 2;
-    for(int i = 3 ; i * i <= MAX_PR ; i++) {
-        if(spf[i] == i) {
-            for(int j = i * i ; j <= MAX_PR ; j+=i) {
-                if(spf[j] == j) spf[j] = i;
+    spf.assign(MAX_PR + 1, 0);
+    iota(all(spf), 0LL);
+    for (int i = 2; i <= MAX_PR; i += 2) spf[i] = 2;
+    for (int i = 3; i * i <= MAX_PR; i++) {
+        if (spf[i] == i) {
+            for (int j = i * i; j <= MAX_PR; j += i) {
+                if (spf[j] == j) spf[j] = i;
             }
         }
     }
+}
+
+vector<int> get_fact(int x) {
+    vector<int> ret;
+    while (x != 1) {
+        ret.push_back(spf[x]);
+        x /= spf[x];
+    }
+    return ret;
 }

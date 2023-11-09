8e2db0ead548f2e2f70abfb05cde19910a100529
Milind Jain <75107993+jainmilind@users.noreply.github.com>
2023-11-09 19:05:13 +0530

    Code Refactoring
    Reformatted code
    Added 1LL wherever necessary as there is a possibility `#define int long long` is not present.

--- Combinatorics/modexp.cpp
+++ Combinatorics/modexp.cpp
@@ -1,11 +1,11 @@
 int modexp(int n, int M, int m = MOD) {
     int retval = 1;
-    while(M > 0) {
-        if(M & 1) {
-            retval = (retval * n) % m;
+    while (M > 0) {
+        if (M & 1) {
+            retval = (1LL * retval * n) % m;
         }
         M = M >> 1;
-        n = (n * n) % m;
+        n = (1LL * n * n) % m;
     }
     return retval % m;
 }

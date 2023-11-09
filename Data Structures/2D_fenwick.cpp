a4235d74f81dd648d44c585051376b6972dac164
Milind Jain <75107993+jainmilind@users.noreply.github.com>
2023-11-09 19:12:31 +0530

    Refactored and add generic types.
    Refactored Code
    Add Generic Types for classes.

--- Data Structures/2D_fenwick.cpp
+++ Data Structures/2D_fenwick.cpp
@@ -1,23 +1,24 @@
+template<typename T>
 struct fenwick_2D {
-    vector<vector<int>>bit;
+    vector<vector<T>> bit;
     int n;
     fenwick_2D(int n) {
         this->n = n;
-        bit.assign(n , vector<int>(n));
+        bit.assign(n, vector<T>(n));
     }
-    void add(int i , int j , int delta) {
+    void add(int i, int j, T delta) {
         for (; i < n; i = i | (i + 1))
             for (int y = j; y < n; y = y | (y + 1))
                 bit[i][y] += delta;
     }
-    int sum(int i , int j) {
-        int val = 0;
+    T sum(int i, int j) {
+        T val{};
         for (; i >= 0; i = (i & (i + 1)) - 1)
             for (int y = j; y >= 0; y = (y & (y + 1)) - 1)
                 val += bit[i][y];
         return val;
     }
-    int sum(int x1 , int y1 , int x2 , int y2) {
+    T sum(int x1, int y1, int x2, int y2) {
         return sum(x2, y2) - sum(x2, y1 - 1) - sum(x1 - 1, y2) + sum(x1 - 1, y1 - 1);
     }
 };
--- Data Structures/fenwick.cpp
+++ Data Structures/fenwick.cpp
@@ -4,21 +4,21 @@ struct fenwick_tree {
     int n;
     fenwick_tree(int n) {
         this->n = n;
-        BIT.assign(n,0);
+        BIT.assign(n, 0);
     }
     void update(int i, T add) {
-        for( ; i < n ; i|=(i+1)) {
+        for (; i < n; i |= (i + 1)) {
             BIT[i] += add;
         }
     }
     T sum(int i) {
         T ans{};
-        for( ; i >= 0 ; i = (i & (i + 1 )) - 1) {
+        for (; i >= 0; i = (i & (i + 1)) - 1) {
             ans += BIT[i];
         }
         return ans;
     }
-    T sum(int l , int r) {
-        return sum(r) - sum(l-1);
+    T sum(int l, int r) {
+        return sum(r) - sum(l - 1);
     }
 };
